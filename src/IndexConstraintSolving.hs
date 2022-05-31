6e4ec1e1927d7890464626385eab475500a2b615
Spidercoder <3743327+MikkelLauridsen@users.noreply.github.com>
2022-05-31 13:26:43 +0900

    Added objective function, fixes

--- src/IndexConstraintSolving.hs
+++ src/IndexConstraintSolving.hs
@@ -13,6 +13,11 @@ import Z3.Monad
 
 data CoefficientConstraint = CCSEqual Coefficient Coefficient | CCSLessEq Coefficient Coefficient | CCSFalse
 
+instance Show CoefficientConstraint where
+  show (CCSEqual a b) = show a ++ " == " ++ show b
+  show (CCSLessEq a b) = show a ++ " <= " ++ show b
+  show CCSFalse = "false"
+
 zeroCoeff = COENumeral 0
 
 reduceIndexConstraints :: [IndexConstraint] -> [CoefficientConstraint]
@@ -27,7 +32,6 @@ reduceIndexConstraints (ICSLessEq env (Index (ix1m, ix1b)) (Index (ix2m, ix2b)) 
     ++ reduceIndexConstraints r
 reduceIndexConstraints (ICSFalse : r) = CCSFalse : reduceIndexConstraints r
 
-
 solveIndexConstraints :: [IndexConstraint] -> IO (Either String (Map.Map CoeffVar Integer))
 solveIndexConstraints constraints = do
   res <- evalZ3 script
@@ -35,18 +39,22 @@ solveIndexConstraints constraints = do
     Just (Sat, vars) -> return $ Right vars
     Just (Unsat, _) -> return $ Left "Unsatisfiable"
     Just (a, vars) -> return $ Left $ "Unknown error: Just (" ++ show a ++ ", " ++ show vars ++ ")"
-    Nothing -> return $ Left "No solution" 
+    Nothing -> return $ Left "No solution"
   where
     coefficientConstraints = reduceIndexConstraints constraints
     script = do
       (asts, vMaps) <- mapM coefficientConstraintToZ3 coefficientConstraints <&> unzip
       let vMaps' = concat vMaps
-      mapM_ assert asts
-      fmap snd $
-        withModel $ \m -> do
-          let (vars, varAsts) = unzip vMaps'
+      let (vars, varAsts) = unzip vMaps'
+      mapM_ optimizeAssert asts
+      getObjectiveFunction vars >>= optimizeMinimize
+      res <- optimizeCheck []
+      case res of
+        Sat -> do
+          m <- optimizeGetModel
           mVals <- mapM (evalInt m) varAsts <&> catMaybes
-          return (Sat, Map.fromList (zip vars mVals))
+          return $ Just (Sat, Map.fromList (zip vars mVals))
+        a -> return $ Just (a, Map.empty)
 
 coefficientConstraintToZ3 :: CoefficientConstraint -> Z3 (AST, [(CoeffVar, AST)])
 coefficientConstraintToZ3 (CCSEqual c1 c2) = do
@@ -63,15 +71,21 @@ coefficientConstraintToZ3 CCSFalse = do
   ast <- mkFalse
   return (ast, [])
 
+getObjectiveFunction :: [CoeffVar] -> Z3 AST
+getObjectiveFunction [] = mkIntNum 0
+getObjectiveFunction (v : vs) = do
+  obj <- getObjectiveFunction vs
+  var <- coefficientVarToZ3 v
+  mkAdd [obj, var]
+
 coefficientToZ3 :: Coefficient -> Z3 (AST, [(CoeffVar, AST)])
-coefficientToZ3 (COEVar c@(CoeffVar v)) = do
-  sym <- mkIntSymbol v
-  ast <- mkIntVar sym
+coefficientToZ3 (COEVar v) = do
+  var <- coefficientVarToZ3 v
   _0 <- mkIntNum 0
-  assert =<< mkGe ast _0
-  return (ast, [(c, ast)])
+  optimizeAssert =<< mkGe var _0
+  return (var, [(v, var)])
 coefficientToZ3 (COENumeral n) = do
-  ast <- mkRealNum 2
+  ast <- mkIntNum n
   return (ast, [])
 coefficientToZ3 (COEAdd c1 c2) = do
   (z1, m1) <- coefficientToZ3 c1
@@ -83,3 +97,8 @@ coefficientToZ3 (COEMul c1 c2) = do
   (z2, m2) <- coefficientToZ3 c2
   ast <- mkMul [z1, z2]
   return (ast, m1 ++ m2)
+
+coefficientVarToZ3 :: CoeffVar -> Z3 AST
+coefficientVarToZ3 (CoeffVar v) = do
+  sym <- mkIntSymbol v
+  mkIntVar sym
